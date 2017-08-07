d34f9520335f54784680b841bdf609ac5eb22a85
arnaz adiputra <arnazadiputra@gmail.com>
2017-08-08 03:33:35 +0700

    add create and delete

--- routes/films.js
+++ routes/films.js
@@ -5,14 +5,44 @@ var Film = require('../models/film')
 
 var router = express.Router()
 
-router.get('/',function(req,res,next) {
-res.json({
-  msg: "Works guys"
+router.delete('/:filmId',function(req,res){
+  var result = {}
+  console.log('menghapus id : ',req.params.filmId)
+  Film.findByIdAndRemove(req.params.filmId,function(err){
+    if(err){
+      result.msg = err
+    }
+    else{
+      result.msg = "delete berhasil"
+    }
+    res.json(result)
   })
+})
+
+router.get('/',function(req,res,next) {
+    Film.find({},function(err,films){
+      if(err){
+        res.json(err)
+      }
+      res.json(films)
+    })
 })
 router.post('/',function(req,res){
   console.log('datanya : ', req.body)
-
-  res.json(req.body)
+  var result  = {}
+  var film = new Film({
+    title: req.body.title,
+    year: req.body.year,
+    released: req.body.released
+  })
+  film.save(function(err){
+    if(err){
+          result.msg = err
+    }
+    else{
+      result.msg = "success"
+    }
+    res.json(result)
+  })
 })
 module.exports = router
